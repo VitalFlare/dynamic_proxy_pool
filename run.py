fd7ecd575622a02f00b217abe266b1f55d945b70
VitalFlare <goodpoutine@gmail.com>
2025-04-25 11:29:16 -0700

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from proxy_manager import ProxyManager
 
 if __name__ == "__main__":
     pm = ProxyManager()
-    pm.run(interval=1800)  # 每半小时更新一次（1800秒）
+    pm.run(interval=900)  # Update every 15 mins (900 seconds).
